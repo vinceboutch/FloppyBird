fb0eddba43a7e9c7e8aa813db969198dad33db3e
vince <vincent.bouchard500@gmail.com>
2018-08-20 20:22:03 -0400

    Second push

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private TranslateMover translateMover;
+        void Awake()
+        {
+            translateMover = GetComponent<TranslateMover>();
+        }
+        // Use this for initialization
+        void Start()
+        {
+            var position = transform.position;
+            Debug.Log("Ma position initiale est : " + position);
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                translateMover.Move(Vector3.up);
+            }
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                translateMover.Move(Vector3.down);
+            }
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                translateMover.Move(Vector3.right);
+            }
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                translateMover.Move(Vector3.left);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/TranslateMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class TranslateMover : MonoBehaviour
+    {
+        [SerializeField] private float speed = 5;
+        public void Move(Vector3 direction)
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+
+    }
+}
