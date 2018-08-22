2dadad9820d30617e2a244d759ec27d20b160af9
vince <vincent.bouchard500@gmail.com>
2018-08-22 14:39:49 -0400

    deplacement done

---
+++ Assets/Scripts/Bird/BirdController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+
+    private BirdPhysics birdPhysics;
+
+    [SerializeField] private KeyCode flapKey = KeyCode.Space;
+
+    private void Awake()
+    {
+        birdPhysics = transform.root.GetComponentInChildren<BirdPhysics>();
+    }
+
+
+    // Update is called once per frame
+    private void Update ()
+    {
+		if( Input.GetKeyDown(flapKey))
+        {
+            birdPhysics.Flap();
+        }
+	}
+}
---
+++ Assets/Scripts/Bird/BirdPhysics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdPhysics : MonoBehaviour {
+
+    private new Rigidbody2D rigidbody;
+    [SerializeField] private float flapForce= 5;
+
+    private void Awake()
+    {
+        rigidbody = transform.root.gameObject.AddComponent<Rigidbody2D>();
+    }
+
+    public void Flap()
+    {
+        rigidbody.velocity = Vector2.up * flapForce;
+    }
+
+}
